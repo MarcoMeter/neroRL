e75e88fe40b592ca345aaa50af26f3cf44cd692f
Marco Pleines <m.pleines@devbeyond.de>
2021-08-27 11:59:52 +0200

    DecoupledPPOTrainer setup

---
+++ neroRL/trainers/policy_gradient/ppo_decoupled.py
@@ -0,0 +1,22 @@
+import torch
+from torch import optim
+
+from neroRL.trainers.policy_gradient.base import BaseTrainer
+from neroRL.utils.utils import masked_mean
+from neroRL.utils.decay_schedules import polynomial_decay
+
+class DecouplePPOTrainer(BaseTrainer):
+    def __init__(self, configs, worker_id, run_id, low_mem_fix, out_path):
+        super().__init__(configs, worker_id, run_id=run_id, low_mem_fix=low_mem_fix, out_path=out_path)
+
+    def train(self):
+        pass
+
+    def step_decay_schedules(self, update):
+        pass
+
+    def collect_checkpoint_data(self, update):
+        return super().collect_checkpoint_data(update)
+
+    def apply_checkpoint_data(self, checkpoint):
+        super().apply_checkpoint_data(checkpoint)
