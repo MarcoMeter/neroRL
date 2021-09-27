cfeee61748b136f5c9f4299cf84832dbbb32fe92
Horrible22232 <sorrybutiwin@web.de>
2021-09-27 22:47:13 +0200

    ++

--- neroRL/nn/encoder.py
+++ neroRL/nn/encoder.py
@@ -190,8 +190,8 @@ class BasicConvBlock(Module):
         """Initializes a two layer residual convolutional neural network like in the DAAC paper.
 
         Arguments:
-            in_channels {int} -- Number of channels in the input image
-            out_channels {int} -- Number of channels produced by the convolution
+            in_channels {int} -- Number of input channels
+            out_channels {int} -- Number of output channels
             stride {int} -- Stride of the convolution. Default: 1
 
         Returns:
