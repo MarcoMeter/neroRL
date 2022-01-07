ec0b5a5bc20f8a8c97bcbd5eff57e34a1c55290d
Horrible22232 <sorrybutiwin@web.de>
2022-01-07 16:33:13 +0100

    dependencies depend on requirements.txt now

--- pypi/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="neroRL",
-    version="0.0.1",
-    packages=find_packages(),
-    install_requires=["numpy", "torch", "gym", "ruamel.yaml", "opencv-python", "matplotlib", "pandas", "scipy", "docopt", "tensorboard==2.0.2", "gym-minigrid==1.0.2", "procgen", "dm-env==1.5", "pycolab==1.2"],
-)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+install_requires = None
+with open("requirements.txt") as file:
+    install_requires = [module_name.rstrip() for module_name in file.readlines()]
+
+setup(
+    name="neroRL",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=install_requires,
+)
