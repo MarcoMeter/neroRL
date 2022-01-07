a302cca1203cfb091f154272935877076d4b8bf5
Horrible22232 <sorrybutiwin@web.de>
2022-01-07 17:05:02 +0100

    work on setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,27 @@ install_requires = None
 with open("requirements.txt") as file:
     install_requires = [module_name.rstrip() for module_name in file.readlines()]
 
+long_description = ""
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="neroRL",
     version="0.0.1",
+    description="A library for Reinforcement Learning in PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarcoMeter/neroRL",
+    project_urls={
+        "Bug Tracker": "https://github.com/MarcoMeter/neroRL/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": ""},
     packages=find_packages(),
-    install_requires=install_requires,
+    python_requires=">=3.6",
+    install_requires=install_requires
 )
