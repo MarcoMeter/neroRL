d2be4350beec6107f48e250dbfaafb49d4942f57
Horrible22232 <sorrybutiwin@web.de>
2022-02-04 15:58:49 +0100

    improved script for generating a pypi package

--- gen_pypi_package.py
+++ gen_pypi_package.py
@@ -2,27 +2,35 @@ import os
 from distutils.dir_util import copy_tree
 import shutil
 
+# Get the current working directory
 cwd = os.getcwd()
+
+# Create a new temporary directory for the package
 if not os.path.exists("tmp"):
     os.makedirs("tmp")
-    
-copy_tree("neroRL", "tmp\\neroRL\\neroRL")
-copy_tree("configs", "tmp\\neroRL\\configs")
-shutil.copy("enjoy.py", "tmp\\neroRL")
-shutil.copy("eval.py", "tmp\\neroRL")
-shutil.copy("train.py", "tmp\\neroRL")
-shutil.copy("eval_checkpoints.py", "tmp\\neroRL")
-shutil.copy("eval.py", "tmp\\neroRL")
-shutil.copy("tune.py", "tmp\\neroRL")
-shutil.copy("neroRL\\__init__.py", "tmp\\neroRL")
-shutil.copy("LICENSE", "tmp")
-shutil.copy("setup.py", "tmp")
-shutil.copy("pyproject.toml", "tmp")
-shutil.copy("requirements.txt", "tmp")
-shutil.copy("README.md", "tmp")
-shutil.copy("MANIFEST.in", "tmp")
+  
+# Files and directories which should be included in the package  
+pypi_folders = ["neroRL", "configs"]
+pypi_files = ["enjoy.py", "eval.py", "train.py", "eval_checkpoints.py", "tune.py", "__init__.py"]
 
-for dname, dirs, files in os.walk("tmp/neroRL"):
+# Setup files to create the package
+setup_files = ["setup.py", "LICENSE", "README.md", "requirements.txt", "MANIFEST.in"]
+
+# Build the package directory
+for folder in pypi_folders:
+    copy_tree(folder, dst="tmp\\neroRL\\" + folder)
+
+for file in pypi_files:
+    shutil.copy(file, "tmp\\neroRL")
+
+for file in setup_files:
+    shutil.copy(file, "tmp")
+
+# Fix imports for the module and delete pycache folders
+for dname, dirs, files in os.walk("tmp\\neroRL"):
+    if "__pycache__" in dname:
+        shutil.rmtree(dname)
+        continue
     for fname in files:
         fpath = os.path.join(dname, fname)
         with open(fpath) as f:
@@ -32,9 +40,13 @@ for dname, dirs, files in os.walk("tmp/neroRL"):
         with open(fpath, "w") as f:
             f.write(s)
   
+# Build the package
 os.chdir("./tmp")          
 os.system("py -m build")
 os.chdir(cwd)
 
+# Copy the package to the current directory
 copy_tree("tmp\\dist", "dist")
+
+# Clean up
 shutil.rmtree("tmp")
