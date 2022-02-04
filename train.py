1fa3446df05e03832abcf01150e127b6157535a9
Horrible22232 <sorrybutiwin@web.de>
2022-02-04 17:07:34 +0100

    Added a flag where the relative part starts from the current module location because it's a lot easier to load the config files when it's installed via pip

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ Runs the training program using the provided config and arguments.
 import random
 
 from docopt import docopt
+import os
 
 from neroRL.utils.yaml_parser import YamlParser
 from neroRL.trainers.policy_gradient.ppo_shared import PPOTrainer
@@ -19,13 +20,19 @@ def main():
 
     Options:
         --config=<path>            Path to the config file [default: ./configs/default.yaml].
+        --use_module_path          Wether the relative path should start from the file location [default: False].
         --worker-id=<n>            Sets the port for each environment instance [default: 2].
         --run-id=<path>            Specifies the tag of the tensorboard summaries [default: default].
         --out=<path>               Specifies the path to output files such as summaries and checkpoints. [default: ./]
         --seed=<n>      	       Specifies the seed to use during training. If set to smaller than 0, use a random seed. [default: -1]
     """
+    
     options = docopt(_USAGE)
-    config_path = options["--config"]
+    if options["--use_module_path"]:
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        config_path = cwd + options["--config"][1:]
+    else:
+        config_path = options["--config"]
     worker_id = int(options["--worker-id"])
     run_id = options["--run-id"]
     out_path = options["--out"]
