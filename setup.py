25dc0e47a7978c2d574651ba86a5105d374f1fca
Horrible22232 <sorrybutiwin@web.de>
2022-03-26 17:46:46 +0100

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 import os
 import sys
-sys.path.insert(0, os.getcwd() + "\\neroRL")
-import __init__ as neroRL
+sys.path.insert(0, os.getcwd())
+import neroRL
 
 # Get current working directory
 cwd = os.getcwd()
