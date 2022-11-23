e6f30d57ac1176e60ff4729128d0e706808a47ae
Marco Pleines <m.pleines@devbeyond.de>
2022-11-23 10:53:29 +0100

    fixed trajectory recording UnityWrapper

--- neroRL/environments/unity_wrapper.py
+++ neroRL/environments/unity_wrapper.py
@@ -187,7 +187,8 @@ class UnityWrapper(Env):
 
         # Prepare trajectory recording
         self._trajectory = {
-            "vis_obs": [vis_obs * 255], "vec_obs": [vec_obs],
+            "vis_obs": [vis_obs * 255] if vis_obs is not None else [vis_obs],
+            "vec_obs": [vec_obs],
             "rewards": [0.0], "actions": []
         }
 
@@ -220,7 +221,10 @@ class UnityWrapper(Env):
 
         # Record trajectory data
         if self._record:
-            self._trajectory["vis_obs"].append(vis_obs * 255)
+            if vis_obs is not None:
+                self._trajectory["vis_obs"].append(vis_obs * 255)
+            else:
+                self._trajectory["vis_obs"].append(vis_obs)
             self._trajectory["vec_obs"].append(vec_obs)
             self._trajectory["rewards"].append(reward)
             self._trajectory["actions"].append(action)
