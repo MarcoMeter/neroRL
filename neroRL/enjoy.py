756ab93370a8771e70c2514e1c03b6eeb08ae075
Horrible22232 <sorrybutiwin@web.de>
2023-01-12 10:40:45 +0100

    ++

--- neroRL/enjoy.py
+++ neroRL/enjoy.py
@@ -20,6 +20,7 @@ from neroRL.utils.utils import get_environment_specs
 from neroRL.environments.wrapper import wrap_environment
 from neroRL.utils.video_recorder import VideoRecorder
 from neroRL.nn.actor_critic import create_actor_critic_model
+from neroRL.nn.wrapper import TruncateMemory
 
 # Setup logger
 logging.basicConfig(level = logging.INFO, handlers=[])
@@ -45,69 +46,6 @@ def init_transformer_memory(trxl_conf, model, device):
     memory_indices = torch.cat((repetitions, memory_indices))
     return memory, memory_mask, memory_indices
 
-class TruncateMemory:
-    def __init__(self, model, mask, model_config, device):
-        """ Truncates the memory of a recurrent or transformer model to the current memory length.
-
-        Arguments:
-            model {ActorCriticSharedWeights}: The model to be truncated. 
-            mask {torch.tensor} -- Memory mask (None if the model is not transformer-based)
-            model_config {dict}: Model config
-            device (_type_): The device to be used.
-        """
-        self.obs = []
-        self.memory_mask = mask
-        if "transformer" in model_config:
-            self.trxl_config = model_config["transformer"]
-            self.memory_length = self.trxl_config["memory_length"]
-        else:
-            self.trxl_config = None
-            self.memory_length = model_config["recurrence"]["sequence_length"]
-        self.model_config = model_config
-        self.model = model
-        self.device = device
-        
-    def add_obs(self, vis_obs, vec_obs):
-        """ Adds an observation to the memory.
-
-        Arguments:
-            vis_obs {numpy.ndarray/torch.tensor} -- Visual observation (None if not available)
-            vec_obs {numpy.ndarray/torch.tensor} -- Vector observation (None if not available)
-        """
-        if len(self.obs) == self.memory_length:
-            self.obs.pop(0)
-        self.obs.append((vis_obs, vec_obs))
-        
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-        
-    def forward(self, vis_obs, vec_obs, _in_memory, _mask, indices):
-        """ Truncates the memory to the current memory length.
-        
-        Arguments:
-            vis_obs {numpy.ndarray/torch.tensor} -- Visual observation (None if not available)
-            vec_obs {numpy.ndarray/torch.tensor} -- Vector observation (None if not available)
-            _in_memory {torch.tensor} -- Not used
-            _mask {torch.tensor} -- Not used
-            indices {torch.tensor} -- Indices to select the positional encoding that matches the memory window (None of the model is not transformer-based)         
-        """
-        self.add_obs(vis_obs, vec_obs)
-        if "recurrence" in self.model_config:
-            in_memory = init_recurrent_cell(self.model_config["recurrence"], self.model, self.device)
-        if "transformer" in self.model_config:
-            in_memory = self.model.init_transformer_memory(1, self.trxl_config["memory_length"], self.trxl_config["num_blocks"], self.trxl_config["embed_dim"], self.device)
-        n = len(self.obs)
-        for i in range(n):
-            vis_obs, vec_obs = self.obs[i]
-            if "recurrence" in self.model_config:
-                policy, value, new_memory, _ = self.model(vis_obs, vec_obs, in_memory, None)
-                in_memory = new_memory
-            if "transformer" in self.model_config:
-                mask = self.memory_mask[i].unsqueeze(0)
-                policy, value, new_memory, _ = self.model(vis_obs, vec_obs, in_memory, mask, indices)
-                in_memory[:, i] = new_memory
-        return policy, value, new_memory, None
-
 def main():
     # Docopt command line arguments
     _USAGE = """
@@ -125,6 +63,7 @@ def main():
         --video=<path>             Specify a path for saving a video, if video recording is desired. The file's extension will be set automatically. [default: ./video].
         --framerate=<n>            Specifies the frame rate of the to be rendered video. [default: 6]
         --generate_website         Specifies wether a website shall be generated. [default: False]
+        --truncate_memory=<n>      Specifies wether the memory should be truncated. [default: False]
     """
     options = docopt(_USAGE)
     untrained = options["--untrained"]                  # defaults to False
@@ -136,6 +75,7 @@ def main():
     video_path = options["--video"]                     # defaults to "video"
     frame_rate = options["--framerate"]                 # defaults to 6
     generate_website = options["--generate_website"]    # defaults to False
+    truncated_memory = options["--truncate_memory"]     # defaults to False
 
     # Determine whether to record a video. A video is only recorded if the video flag is used.
     record_video = False
@@ -220,7 +160,8 @@ def main():
         logger.info("Step 4: Run " + str(num_episodes) + " episode(s) in realtime . . .")
 
         # Truncates the memory of the model
-        model = TruncateMemory(model, memory_mask, model_config, device)
+        if truncated_memory:
+            model = TruncateMemory(model, memory_mask, model_config, device)
 
         # Store data for video recording
         probs, entropies, values, actions = [], [], [], []
---
+++ neroRL/nn/wrapper.py
@@ -0,0 +1,79 @@
+class TruncateMemory:
+    def __init__(self, model, mask, model_config, device):
+        """ Truncates the memory of a recurrent or transformer model to the current memory length.
+
+        Arguments:
+            model {ActorCriticSharedWeights}: The model to be truncated. 
+            mask {torch.tensor} -- Memory mask (None if the model is not transformer-based)
+            model_config {dict}: Model config
+            device (_type_): The device to be used.
+        """
+        self.obs = []
+        self.memory_mask = mask
+        if "transformer" in model_config:
+            self.trxl_config = model_config["transformer"]
+            self.memory_length = self.trxl_config["memory_length"]
+        else:
+            self.trxl_config = None
+            self.memory_length = model_config["recurrence"]["sequence_length"]
+        self.model_config = model_config
+        self.model = model
+        self.device = device
+        
+    def add_obs(self, vis_obs, vec_obs):
+        """ Adds an observation to the memory.
+
+        Arguments:
+            vis_obs {numpy.ndarray/torch.tensor} -- Visual observation (None if not available)
+            vec_obs {numpy.ndarray/torch.tensor} -- Vector observation (None if not available)
+        """
+        # Remove oldest observation if memory is full
+        if len(self.obs) == self.memory_length:
+            self.obs.pop(0)
+        self.obs.append((vis_obs, vec_obs))
+        
+    def __call__(self, *args, **kwargs):
+        return self.forward(*args, **kwargs)
+    
+    def init_recurrent_cell(self, recurrence_config, model, device):
+        hxs, cxs = model.init_recurrent_cell_states(1, device)
+        if recurrence_config["layer_type"] == "gru":
+            recurrent_cell = hxs
+        elif recurrence_config["layer_type"] == "lstm":
+            recurrent_cell = (hxs, cxs)
+        return recurrent_cell
+        
+    def forward(self, vis_obs, vec_obs, _in_memory, _mask, indices):
+        """ Truncates the memory to the current memory length.
+        
+        Arguments:
+            vis_obs {numpy.ndarray/torch.tensor} -- Visual observation (None if not available)
+            vec_obs {numpy.ndarray/torch.tensor} -- Vector observation (None if not available)
+            _in_memory {torch.tensor} -- Not used
+            _mask {torch.tensor} -- Not used
+            indices {torch.tensor} -- Indices to select the positional encoding that matches the memory window (None of the model is not transformer-based)  
+            
+        Returns:
+            {list} -- Policy: List featuring categorical distributions respectively for each policy branch
+            {torch.tensor} -- Value function: Value
+            {torch.tensor or tuple} -- Current memory representation or recurrent cell state (None if memory is not used)       
+        """
+        # Add new observation to memory
+        self.add_obs(vis_obs, vec_obs)
+        # Create new memory
+        if "recurrence" in self.model_config:
+            in_memory = self.init_recurrent_cell(self.model_config["recurrence"], self.model, self.device)
+        if "transformer" in self.model_config:
+            in_memory = self.model.init_transformer_memory(1, self.trxl_config["memory_length"], self.trxl_config["num_blocks"], self.trxl_config["embed_dim"], self.device)
+        # Recompute the policy, value and memory with the truncated episode
+        n = len(self.obs)
+        for i in range(n):
+            vis_obs, vec_obs = self.obs[i]
+            if "recurrence" in self.model_config:
+                policy, value, new_memory, _ = self.model(vis_obs, vec_obs, in_memory, None)
+                in_memory = new_memory
+            if "transformer" in self.model_config:
+                mask = self.memory_mask[i].unsqueeze(0)
+                policy, value, new_memory, _ = self.model(vis_obs, vec_obs, in_memory, mask, indices)
+                in_memory[:, i] = new_memory
+        return policy, value, new_memory, None
